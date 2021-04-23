6c7b2d1745c652bf659b8dfdd2bff56a439fbba4
cayleatopolski <woahitscaylea@gmail.com>
2021-04-23 11:34:59 -0400

    Added toggle switch.

--- pages/components/common/ModeToggle.js
+++ pages/components/common/ModeToggle.js
@@ -1,5 +1,94 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  margin-right: 5px;
+`;
+
+const Label = styled.label`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 42px;
+  height: 26px;
+  border-radius: 15px;
+  background: #5ff6f6;
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    display: block;
+    border-radius: 50%;
+    width: 18px;
+    height: 18px;
+    margin: 3px;
+    background: #fff;
+    box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
+  }
+`;
+
+const Input = styled.input`
+  opacity: 0;
+  z-index: 1;
+  border-radius: 15px;
+  width: 42px;
+  height: 26px;
+
+  &:checked + ${Label} {
+    background: #4b3085;
+
+    &::after {
+      content: '';
+      display: block;
+      border-radius: 50%;
+      width: 18px;
+      height: 18px;
+      margin-left: 21px;
+      transition: 0.2s;
+    }
+  }
+`;
+
+const Span = styled.span`
+  /* position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+
+  &:before {
+    position: absolute;
+    content: '';
+    height: 40px;
+    width: 40px;
+    left: 0px;
+    bottom: 4px;
+    top: 0;
+    bottom: 0;
+    margin: auto 0;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+    box-shadow: 0 0px 15px #2020203d;
+    background: white url('https://i.ibb.co/FxzBYR9/night.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    border-radius: 50%;
+  } */
+`;
+
 const ModeToggle = ({ toggleTheme }) => {
-  return <button onClick={toggleTheme}>Switch Theme</button>;
+  return (
+    <Container>
+      <Input id='checkbox' type='checkbox' onChange={toggleTheme} />
+      <Label htmlFor='checkbox'></Label>
+    </Container>
+  );
 };
 
 export default ModeToggle;
